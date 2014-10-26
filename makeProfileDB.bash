22b0b04ce6ce27d0d50d623d7acae370d07b8d5a
gg_hatano <adad0405@gmail.com>
2014-10-27 00:13:09 +0900

    プロフィール作ります

---
+++ makeProfileDB.bash
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+for profile in `cat ./va_link.txt`
+do
+  curl $profile | 
+  grep -A 6 "名前" |  
+  tr -d "\n"  | 
+  sed -e 's/<[^>]*>/;/g' -e 's/\s\s*/\s/g' |
+  awk -F ";" -v OFS=";" '{print $4, $8, $12,$16,$20,$24,$28,$32,$40}' >> res.txt
+done
